f853585d73a276cba45a7287b30b77b3673cbb77
Mohamed-Amine <145483177+medAmine12558@users.noreply.github.com>
2024-11-08 21:37:04 +0100

    v2

--- routes/web.php
+++ routes/web.php
@@ -7,24 +7,24 @@ use Inertia\Inertia;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Middleware\ForceHttps;
 
-Route::get('/a', function () {
+/*'Route::get('/a', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+});*/
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
 
-Route::middleware('auth')->group(function () {
+/*Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 
     Route::get('/',[AppointmentController::class,'homepage'])->name('homepage');
