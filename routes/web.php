5d28fa534b26c6519edd6088eb1acf983ffbfddf
Mohamed-Amine <145483177+medAmine12558@users.noreply.github.com>
2024-11-08 22:02:16 +0100

    v2

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ use App\Http\Middleware\ForceHttps;
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');*/
 
-/*Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});*/
+});
 
 
     Route::get('/',[AppointmentController::class,'homepage'])->name('homepage');
